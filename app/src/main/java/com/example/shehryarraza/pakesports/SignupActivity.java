2794d079bdf8065528662bc93c1cb28b305a8082
ShehryarRaza <shehryarraza320@gmail.com>
2018-08-10 20:34:27 +0500

    State added

--- app/src/main/java/com/example/shehryarraza/pakesports/SignupActivity.java
+++ app/src/main/java/com/example/shehryarraza/pakesports/SignupActivity.java
@@ -2,32 +2,71 @@ package com.example.shehryarraza.pakesports;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 public class SignupActivity extends AppCompatActivity {
-    private Button button;
+
+    private Button button2;
     private EditText editText;
     private EditText DOB;
+    private EditText state;
+    private Spinner spinner;
+    EditText textview_countries;
+
+    private String[] countries_list={"Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"};
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-
         setContentView(R.layout.activity_signup);
 
         editText =  findViewById(R.id.fullname);
-
         DOB = findViewById(R.id.DOB);
+        spinner = findViewById(R.id.spinner);
+        textview_countries= findViewById(R.id.txtview_countries);
+        textview_countries.setInputType(InputType.TYPE_NULL);
+
+        final ArrayAdapter<String> spinner_countries = new  ArrayAdapter<String>(SignupActivity.this,android.R.layout.simple_spinner_dropdown_item, countries_list);
+
+        textview_countries.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                new AlertDialog.Builder(SignupActivity.this)
+                        .setTitle("Select Countries")
+                        .setAdapter(spinner_countries, new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int which) {
+                                textview_countries.setText(countries_list[which].toString());
+                                dialog.dismiss();
+                            }
+                        }).create().show();
+            }
+        });
+
 
 
         DOB.setOnClickListener(new View.OnClickListener() {
@@ -70,9 +109,9 @@ public class SignupActivity extends AppCompatActivity {
     };
 
     private void updateLabel() {
-        String myFormat = "dd/MM/yy"; //In which you need put here
-        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-        DOB.setText(sdf.format(myCalendar.getTime()));
+
+        DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.ENGLISH);
+        DOB.setText(df.format(myCalendar.getTime()));
     }
 
 
