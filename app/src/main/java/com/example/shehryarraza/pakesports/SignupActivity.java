5d28b0b5700e7263f0d9b8875392b81f5cb4eb32
ShehryarRaza <shehryarraza320@gmail.com>
2018-08-10 16:40:08 +0500

    Date of Birth Added

--- app/src/main/java/com/example/shehryarraza/pakesports/SignupActivity.java
+++ app/src/main/java/com/example/shehryarraza/pakesports/SignupActivity.java
@@ -1,14 +1,22 @@
 package com.example.shehryarraza.pakesports;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class SignupActivity extends AppCompatActivity {
     private Button button;
     private EditText editText;
+    private EditText DOB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +26,22 @@ public class SignupActivity extends AppCompatActivity {
 
         editText =  findViewById(R.id.fullname);
 
+        DOB = findViewById(R.id.DOB);
+
+
+        DOB.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(SignupActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+
     }
     @Override
     public void onBackPressed() {
@@ -25,4 +49,28 @@ public class SignupActivity extends AppCompatActivity {
 
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
+
+    Calendar myCalendar = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        DOB.setText(sdf.format(myCalendar.getTime()));
+    }
+
+
 }
