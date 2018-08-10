0543cbb0f1264f3d51774d3667ab727af970d55d
ShehryarRaza <shehryarraza320@gmail.com>
2018-08-10 12:03:44 +0500

    Testing

--- app/src/main/java/com/example/shehryarraza/pakesports/MainActivity.java
+++ app/src/main/java/com/example/shehryarraza/pakesports/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
 
             }
         });
