461a27448ee9c44a34955488a4f949f0be173667
ShehryarRaza <shehryarraza320@gmail.com>
2018-08-12 07:26:55 +0500

    Login and Sign-up Activities UI Done!

--- app/src/main/java/com/example/shehryarraza/pakesports/SignupActivity.java
+++ app/src/main/java/com/example/shehryarraza/pakesports/SignupActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.content.res.AppCompatResources;
 import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,11 +17,14 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -30,12 +34,38 @@ public class SignupActivity extends AppCompatActivity {
     private EditText editText;
     private EditText DOB;
     private EditText state;
-    private Spinner spinner;
+    private EditText password;
+    private EditText confirm_password;
+    private SearchableSpinner spinner;
     EditText textview_countries;
+    boolean flag = false;        // Here
 
-    private String[] countries_list={"Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"};
+    private String[] countries_list={"Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan",
+                                     "Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi",
+                                     "Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic",
+                                     "Denmark","Djibouti","Dominica","Dominican Republic",
+                                     "Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia",
+                                     "Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies",
+                                     "Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana",
+                                     "Haiti","Honduras","Hong Kong","Hungary",
+                                     "Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy",
+                                     "Jamaica","Japan","Jersey","Jordan",
+                                     "Kazakhstan","Kenya","Kuwait","Kyrgyz Republic",
+                                     "Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg",
+                                     "Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique",
+                                     "Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman",
+                                     "Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico",
+                                     "Qatar",
+                                     "Reunion","Romania","Russia","Rwanda",
+                                     "Saint Pierre & Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria",
+                                     "Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos",
+                                     "Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan",
+                                     "Venezuela","Vietnam","Virgin Islands (US)",
+                                     "Yemen",
+                                     "Zambia","Zimbabwe",""};
 
 
+    final int listsize = countries_list.length - 1;
 
 
     @Override
@@ -45,38 +75,71 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         editText =  findViewById(R.id.fullname);
+        password = findViewById(R.id.password);
+        confirm_password = findViewById(R.id.confirmpassword);
         DOB = findViewById(R.id.DOB);
         spinner = findViewById(R.id.spinner);
         textview_countries= findViewById(R.id.txtview_countries);
         textview_countries.setInputType(InputType.TYPE_NULL);
 
-        final ArrayAdapter<String> spinner_countries = new  ArrayAdapter<String>(SignupActivity.this,android.R.layout.simple_spinner_dropdown_item, countries_list);
 
-        textview_countries.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
-                new AlertDialog.Builder(SignupActivity.this)
-                        .setTitle("Select Countries")
-                        .setAdapter(spinner_countries, new DialogInterface.OnClickListener() {
-
-                            public void onClick(DialogInterface dialog, int which) {
-                                textview_countries.setText(countries_list[which].toString());
-                                dialog.dismiss();
-                            }
-                        }).create().show();
+        final CustomAdapter<String> spinner_countries = new CustomAdapter<String>(SignupActivity.this,android.R.layout.simple_spinner_item, countries_list){
+            @Override
+            public int getCount() {
+                return(listsize); // Truncate the list
             }
+        };
+
+        spinner.setTitle("Select Country");
+        spinner.setAdapter(spinner_countries);
+        spinner.setSelection(listsize); // Hidden item to appear in the spinner
+
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String hovno = spinner.getSelectedItem().toString();
+                textview_countries.setText(hovno);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
         });
 
 
+
+        final DatePickerDialog datePickerDialog = new DatePickerDialog(SignupActivity.this, android.R.style.Theme_Holo_Dialog_MinWidth,date, myCalendar
+                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH));
 
         DOB.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+
                 // TODO Auto-generated method stub
-                new DatePickerDialog(SignupActivity.this, android.R.style.Theme_Holo_Light_Dialog,date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+                Calendar mindate = Calendar.getInstance();
+
+                Calendar maxdate = Calendar.getInstance();
+
+                mindate.set(1970, 0, 1);//Year,Mounth -1,Day
+                maxdate.set(2017, 11, 31);//Year,Mounth -1,Day
+
+                datePickerDialog.getDatePicker().setMinDate(mindate.getTimeInMillis());
+                datePickerDialog.getDatePicker().setMaxDate(maxdate.getTimeInMillis());
+
+
+                datePickerDialog.show();
+
+
 
 
             }
